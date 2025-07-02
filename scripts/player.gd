9b5573216749a78d19e88130af98faec0eaf9297
dev <dev@dev.com>
2025-07-02 16:41:43 -0300

    Adicionando projeto ao git

---
+++ scripts/player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var move_speed = 400
+var gravity = 1200
+var jump_force = -720
+
+func _process(delta: float) -> void:
+	velocity.y += gravity * delta
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
+	velocity.x = move_speed * move_direction
+	
+	move_and_slide(velocity)
