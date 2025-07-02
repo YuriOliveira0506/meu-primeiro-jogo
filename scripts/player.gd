37fdf60ba28bba9cfa998e560c2ebe902438a2aa
dev <dev@dev.com>
2025-07-02 18:02:18 -0300

    construção do cenário(tilemap)

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,9 @@ func _process(delta: float) -> void:
 	velocity.y += gravity * delta
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
+	if Input.is_action_pressed("jump"):
+		velocity.y = jump_force /3
+		
 	velocity.x = move_speed * move_direction
 	
 	move_and_slide(velocity)
