5084f081dbdc04474525b6f7e5e9a892ae81ab2d
dev <dev@dev.com>
2025-07-23 18:01:26 -0300

    scripts e animações do Player

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,14 +4,22 @@ var velocity = Vector2.ZERO
 var move_speed = 400
 var gravity = 1200
 var jump_force = -720
+var is_grounded
+onready var raycasts = $raycasts
 
 func _process(delta: float) -> void:
 	velocity.y += gravity * delta
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
+
+	_get_input()
+
 	if Input.is_action_pressed("jump"):
 		velocity.y = jump_force / 2
 	
+	move_and_slide(velocity)
+
+func _get_input():
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.x = move_speed * move_direction
 	
-	move_and_slide(velocity)
+	if move_direction != 0:
+		$texture.scale.x = move_direction
