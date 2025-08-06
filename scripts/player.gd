979aef485e6b00867f83dde036d88af12cc4307c
YuriOliveira0506 <ryuri483@gmail.com>
2025-08-06 17:32:06 -0300

    camera 2d

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,51 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var move_speed = 400
+var move_speed = 750
+
 var gravity = 1200
 var jump_force = -720
-var is_grounded
+var is_grounded = false
 onready var raycasts = $raycasts
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 
 	_get_input()
 
-	if Input.is_action_pressed("jump"):
-		velocity.y = jump_force / 2
-	
-	move_and_slide(velocity)
+	move_and_slide(velocity, Vector2.UP)
 
-func _get_input():
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	velocity.x = move_speed * move_direction
+	is_grounded = _check_is_grounded()
 	
+	_set_animation()
+func _get_input():
+	velocity.x = 0
+	
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
+
 	if move_direction != 0:
 		$texture.scale.x = move_direction
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") and is_grounded:
+		velocity.y = jump_force / 2
+
+func _check_is_grounded() -> bool:
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+	return false
+
+func _set_animation():
+	var anim = "idle"
+
+	if !is_grounded:
+		anim = "jump"
+
+	elif velocity.x != 0:
+		anim = "run"
+		
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
+
